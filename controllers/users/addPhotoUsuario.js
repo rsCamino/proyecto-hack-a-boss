594872387a1423b444f1e7e01d26c61d02295ef6
Candi87 <kndy_1987@hotmail.com>
2021-06-25 17:39:35 +0200

    endpoint de addphotouser

---
+++ controllers/users/addPhotoUsuario.js
@@ -0,0 +1,50 @@
+const getDB = require('../../ddbb/db');
+
+const { savePhoto, formatDate } = require('../../helpers');
+
+const addPhotoUsuario = async (req, res, next) => {
+	let connection;
+
+	try {
+		connection = await getDB();
+
+		const { idUsuario } = req.params;
+		const { description } = req.body;
+
+		if (req.authEntity.idUsuario !== Number(idUsuario)) {
+			const error = new Error(
+				'No tienes permisos para subir fotos en el perfil de este usuario'
+			);
+			error.httpStatus = 403;
+			throw error;
+		}
+
+		let photoName;
+
+		const now = formatDate(new Date());
+
+		if (req.files && req.files.photo) {
+			photoName = await savePhoto(req.files.photo);
+
+			await connection.query(
+				`INSERT INTO imagenes (imagen, descripcion, idUsuario, fechasubida) VALUES (?, ?, ?, ?);`,
+				[photoName, description, idUsuario, now]
+			);
+		}
+
+		res.send({
+			status: 'ok',
+			data: {
+				foto: photoName,
+				descripcion: description,
+				fechaDeSubida: now,
+			},
+		});
+	} catch (error) {
+		next(error);
+	} finally {
+		if (connection) connection.release();
+	}
+};
+
+module.exports = addPhotoUsuario;
