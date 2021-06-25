eb0b254e6d5ff4007468c343fda862c47f09758c
rsCamino <86075603+rsCamino@users.noreply.github.com>
2021-06-25 18:57:04 +0200

    Create deleteFotoEstablecimiento.js
    Se arregla el archivo

---
+++ controllers/establecimientos/deleteFotoEstablecimiento.js
@@ -0,0 +1,57 @@
+const getDB = require('../../ddbb/db');
+
+const { deletePhoto } = require('../../helpers');
+
+const deleteFotoEstablecimiento = async (req, res, next) => {
+	let connection;
+
+	try {
+		connection = await getDB();
+
+		const { idEstablecimiento, idPhoto } = req.params;
+
+		if (
+			req.authEntity.idEstablecimiento !== Number(idEstablecimiento) &&
+			req.authEntity.role !== 'admin'
+		) {
+			const error = new Error(
+				'No tienes permisos para eliminar fotos de este establecimiento'
+			);
+			error.httpStatus = 401;
+			throw error;
+		}
+
+		const [photo] = await connection.query(
+			`SELECT imagen FROM imagenes WHERE idEstablecimiento = ?;`,
+			[idEstablecimiento]
+		);
+
+		if (photo.length < 1) {
+			const error = new Error('La foto no existe');
+			error.httpStatus = 404;
+			throw error;
+		}
+
+		console.log(photo);
+		const uuid = Number(idPhoto);
+
+		// Borramos la foto del servidor.
+		await deletePhoto(photo[uuid - 1].imagen);
+
+		// Borramos la foto de la base de datos.
+		await connection.query(`DELETE FROM imagenes WHERE imagen = ?;`, [
+			photo[uuid - 1].imagen,
+		]);
+
+		res.send({
+			status: 'Ok',
+			message: 'Foto eliminada',
+		});
+	} catch (error) {
+		next(error);
+	} finally {
+		if (connection) connection.release();
+	}
+};
+
+module.exports = deleteFotoEstablecimiento;
