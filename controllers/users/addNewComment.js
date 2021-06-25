c565c8e92bd05cef8916c67331a40e8d943f57ea
mikel <mikel.remesar@gmail.com>
2021-06-25 19:35:01 +0200

    añadido endpoint eComentar Foto

---
+++ controllers/users/addNewComment.js
@@ -0,0 +1,38 @@
+const getDB = require('../../ddbb/db');
+const { formatDate } = require('../../helpers');
+
+const addNewComment = async (req, res, next) => {
+	let connection;
+	try {
+		connection = await getDB();
+
+		const { comentario, idImagen } = req.body;
+		const { idUsuario } = req.authEntity;
+		const now = new Date();
+		const [texto] = await connection.query(
+			`
+            INSERT INTO usuarios_imagenes ( comentario, idImagen, idUsuario)
+            VALUES (?,?,?);
+            `,
+			[comentario, Number(idImagen), idUsuario]
+		);
+		const { insertID: idcomment } = addNewComment;
+
+		res.send({
+			status: 'ok',
+			data: {
+				id: idcomment,
+				idUsuario,
+				comentario,
+				idImagen,
+				creadoEn: now,
+				likes: 0,
+			},
+		});
+	} catch (error) {
+		next(error);
+	} finally {
+		if (connection) connection.release;
+	}
+};
+module.exports = addNewComment;
