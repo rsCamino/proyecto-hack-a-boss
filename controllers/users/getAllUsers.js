c7e870e7ff55048a3b376b78f151a9d7a29b83b8
rsCamino <86075603+rsCamino@users.noreply.github.com>
2021-08-27 19:14:23 +0200

    Endpoint para mostrar todos los usuarios

---
+++ controllers/users/getAllUsers.js
@@ -0,0 +1,41 @@
+const getDB = require('../../ddbb/db');
+const { PUBLIC_HOST } = process.env;
+
+const getAllUsers = async (req, res, next) => {
+	let connection;
+
+	try {
+		connection = await getDB();
+
+		const [usuario] = await connection.query(
+			`SELECT id, nombre, nickname, fotoperfil FROM usuarios;`,
+			[]
+		);
+    
+    let usersInfo = [];
+    for (let i = 0; i < usuario.length; i++) {
+    if (usuario[i].deleted !== 0) {
+    usersInfo.push({
+    name: usuario[i].nombre,
+    nickname: usuario[i].nickname,
+    fotoperfil: `${PUBLIC_HOST}/uploads/${usuario[i].fotoperfil}`, })
+    };
+    }
+			res.send({
+				status: 'ok',
+				informacion: usersInfo,
+			});
+		} else {
+			res.send({
+				status: 'Deleted',
+				data: 'No hay usuarios en la base de datos',
+			});
+		}
+	} catch (error) {
+		next(error);
+	} finally {
+		if (connection) connection.release();
+	}
+};
+
+module.exports = getAllUsers;
